344b9ca5f7b1364ec00d3f6209c7b655f74ef954
LA-DI-DA <11174151+0O0o0oOoO00@users.noreply.github.com>
2023-10-11 10:25:14 +0800

    Add: ship attribute data

---
+++ Tools/ShipAttribute.py
@@ -0,0 +1,32 @@
+from JsonToCsv import JsonToCsv
+
+RAWDATA_FILE = "../RawData/ship_data_statistics.json"
+DATA_FILE = "../Data/ShipAttribute.csv"
+
+BLACK_ROOT_KEY = ["all"]
+
+DATA_SPACE_TO_WRITE = {
+    "名称": "name",
+    "星级": "star",
+    "耐久": "attrs.0",
+    "炮击": "attrs.1",
+    "雷击": "attrs.2",
+    "防空": "attrs.3",
+    "航空": "attrs.4",
+    "装填": "attrs.5",
+    "反潜": "attrs.6",
+    "命中": "attrs.7",
+    "机动": "attrs.8",
+    "耐久(增长)": "attrs_growth.0",
+    "炮击(增长)": "attrs_growth.1",
+    "雷击(增长)": "attrs_growth.2",
+    "防空(增长)": "attrs_growth.3",
+    "航空(增长)": "attrs_growth.4",
+    "装填(增长)": "attrs_growth.5",
+    "反潜(增长)": "attrs_growth.6",
+    "命中(增长)": "attrs_growth.7",
+    "机动(增长)": "attrs_growth.8"
+}
+
+if __name__ == "__main__":
+    JsonToCsv(RAWDATA_FILE, DATA_SPACE_TO_WRITE, BLACK_ROOT_KEY).WriteToFile(DATA_FILE)
