a7edd7ae6ce405fc70d59f1dfa11dc9b785a928d
LA-DI-DA <11174151+0O0o0oOoO00@users.noreply.github.com>
2023-10-21 20:08:51 +0800

    Fix: repeatedly useless searching and bitwise operation

--- 改属性(适配多种模拟器).lua
+++ 改属性(适配多种模拟器).lua
@@ -37,27 +37,8 @@ function ChangeShipAttribute()
 		end
 
 		for j = 1, #SearchResult do
-			local MemoryFrom = SearchResult[j].address - 0x1850
-			local MemoryTo = SearchResult[j].address
-			for k = 1, #ShipAttributeList do
-				local SplitedAttribute = string.split(tostring(ShipAttributeList[k]), ">")
-				local AttributeToSearchList = SplitedAttribute[1]
-				local TargetAttributeList = SplitedAttribute[2]
-				gg.clearResults()
-				gg.searchNumber(AttributeToSearchList, gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, MemoryFrom, MemoryTo)
-				local AttributeResult = gg.getResults(1024)
-				if next(AttributeResult) == nil then
-					gg.clearResults()
-					gg.searchNumber(AttributeToSearchList, gg.TYPE_DWORD, false, gg.SIGN_EQUAL, MemoryFrom, MemoryTo)
-					gg.getResults(1024)
-					gg.editAll(TargetAttributeList, gg.TYPE_DWORD)
-				else
-					gg.editAll(TargetAttributeList, gg.TYPE_DOUBLE)
-				end
-				gg.clearResults()
-			end
-			MemoryFrom = SearchResult[j].address - 0x300
-			MemoryTo = SearchResult[j].address + 0x300
+			local MemoryFrom = SearchResult[j].address - 0x300
+			local MemoryTo = SearchResult[j].address + 0x300
 			local Base1 = SearchResult[j].address & 0xF0000000
 			local Base2 = SearchResult[j].address | 0xFFFFFFF
 			gg.searchNumber(Base1.."~"..Base2.."", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, MemoryFrom, MemoryTo)
@@ -67,19 +48,20 @@ function ChangeShipAttribute()
 				AddressResult = gg.getResults(1024)
 			end
 			for l = 1, #AddressResult do
-				MemoryFrom = AddressResult[l].value
-				MemoryTo = AddressResult[l].value + 0x100
+				MemoryFrom = AddressResult[l].value & 0xFFFFFFFF
+				MemoryTo = AddressResult[l].value & 0xFFFFFFFF
+				MemoryTo = MemoryTo + 0x90
 				for m = 1, #ShipAttributeList do
 					local SplitedAttribute = string.split(tostring(ShipAttributeList[m]), ">")
 					local AttributeToSearchList = SplitedAttribute[1]
 					local TargetAttributeList = SplitedAttribute[2]
 					gg.clearResults()
 					gg.searchNumber(AttributeToSearchList, gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, MemoryFrom, MemoryTo)
-					local AttributeResult = gg.getResults(1024)
+					local AttributeResult = gg.getResults(1)
 					if next(AttributeResult) == nil then
 						gg.clearResults()
 						gg.searchNumber(AttributeToSearchList, gg.TYPE_DWORD, false, gg.SIGN_EQUAL, MemoryFrom, MemoryTo)
-						gg.getResults(1024)
+						AttributeResult = gg.getResults(1)
 						gg.editAll(TargetAttributeList, gg.TYPE_DWORD)
 					else
 						gg.editAll(TargetAttributeList, gg.TYPE_DOUBLE)
